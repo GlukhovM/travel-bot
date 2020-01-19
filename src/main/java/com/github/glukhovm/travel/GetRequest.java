7d205730009a08443372abb67d94f20746815363
Mikhail <51836760+glukhovm@users.noreply.github.com>
2020-01-19 22:42:38 +0500

    TB-3: package fix

--- src/main/java/com/github/glukhovm/travel/GetRequest.java
+++ src/main/java/com/github/glukhovm/travel/GetRequest.java
@@ -1,3 +1,5 @@
+package com.github.glukhovm.travel;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -6,7 +8,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 public class GetRequest {
-    private static String requestURL = "https://api.skypicker.com/flights?fly_from=CEK&fly_to=LED&date_from=01/06/2020&date_to=01/07/2020&partner=picky&curr=RUB";
+    private static String requestURL = "https://api.skypicker.com/flights?fly_from=CEK&fly_to=LED&date_from=01/06/2020&date_to=01/07/2020&partner=picky&curr=RUB&limit=1";
 
     public static void main(String[] args) {
         try (
@@ -14,12 +16,14 @@ public class GetRequest {
                 CloseableHttpResponse response = client.execute(new HttpGet(requestURL))
         ) {
             System.out.println("Status line: " + response.getStatusLine());
+
             HttpEntity entity = response.getEntity();
 
             if (entity != null) {
-                String data = IOUtils.toString(entity.getContent());
+                String data = IOUtils.toString(entity.getContent(),"cp1251");
                 System.out.println("Data :" + data);
             }
+
         } catch (Throwable cause) {
           cause.printStackTrace();
         }
