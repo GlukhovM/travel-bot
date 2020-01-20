3a8d92c3a5db2e58f5564a9c67596581f728b7e7
Mikhail <51836760+glukhovm@users.noreply.github.com>
2020-01-21 01:00:12 +0500

    TB-4: first commit

--- src/main/java/com/github/glukhovm/travel/GetRequest.java
+++ src/main/java/com/github/glukhovm/travel/GetRequest.java
@@ -1,5 +1,7 @@
 package com.github.glukhovm.travel;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -8,7 +10,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 public class GetRequest {
-    private static String requestURL = "https://api.skypicker.com/flights?fly_from=CEK&fly_to=LED&date_from=01/06/2020&date_to=01/07/2020&partner=picky&curr=RUB&limit=1";
+    private static String requestURL = "https://api.skypicker.com/flights?fly_from=CEK&fly_to=LED&date_from=01/06/2020&date_to=01/07/2020&partner=picky&curr=RUB&limit=2";
 
     public static void main(String[] args) {
         try (
@@ -19,10 +21,17 @@ public class GetRequest {
 
             HttpEntity entity = response.getEntity();
 
+            // add status 200 verification from point 2 TB-2!!!
+
             if (entity != null) {
                 String data = IOUtils.toString(entity.getContent(),"cp1251");
                 System.out.println("Data :" + data);
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                ResponseDto responseDto = objectMapper.readValue(data, ResponseDto.class);
+                System.out.println(responseDto);
             }
+
 
         } catch (Throwable cause) {
           cause.printStackTrace();
