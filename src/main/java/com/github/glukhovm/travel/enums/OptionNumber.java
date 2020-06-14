76048ab65ee2f1fd1c859195d52eb9aba909f2e1
Mikhail <livestrong174@gmail.com>
2020-06-15 02:55:23 +0500

    dev: fix NPE enum bug

--- src/main/java/com/github/glukhovm/travel/enums/OptionNumber.java
+++ src/main/java/com/github/glukhovm/travel/enums/OptionNumber.java
@@ -1,11 +1,12 @@
 package com.github.glukhovm.travel.enums;
 
-public enum  OptionNumber {
+import java.util.Objects;
+
+public enum OptionNumber {
     SELECTION_ALL_FOUR_PARAMETERS("1"),
     SELECTION_AIRPORTS_ONLY("2"),
     SELECTION_DEPARTURE_AIRPORT_ONLY("3"),
     UNKNOWN_OPTION(null);
-
 
     private final String optionNumber;
 
@@ -13,10 +14,10 @@ public enum  OptionNumber {
         this.optionNumber = optionNumber;
     }
 
-    public static OptionNumber byCode(String optionNumber){
-        for(OptionNumber value : values()){
-        if(value.optionNumber.equals(optionNumber)) {
-            return value;
+    public static OptionNumber byCode(String optionNumber) {
+        for (OptionNumber value : values()) {
+            if (Objects.equals(value.optionNumber, optionNumber)) {
+                return value;
             }
         }
         return UNKNOWN_OPTION;
